15e212dc56e93eb682f0aa428a97abb5f22d2655
Carmofrasao <anderfrasao52@gmail.com>
2022-04-22 23:51:17 -0300

    ..

--- trab2-icc-opt/exec_op.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-likwid-perfctr -C 3 -g L3 -m -O ./newtonPC < funcoes.dat > L3-opt.log
-likwid-perfctr -C 3 -g L2CACHE -m -O ./newtonPC < funcoes.dat > L2CACHE-opt.log
-likwid-perfctr -C 3 -g FLOPS_DP -m -O ./newtonPC < funcoes.dat > FLOPS_DP-opt.log
-likwid-perfctr -C 3 -g CLOCK -m -O ./newtonPC < funcoes.dat > CLOCK-opt.log
-
